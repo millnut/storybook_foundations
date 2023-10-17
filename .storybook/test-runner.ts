e244fe3c4bc0838e27762b482561853a9e88d68a
Lee Mills <8024370+millnut@users.noreply.github.com>
2023-10-17 15:59:00 +0100

    ci: add accessibility testing to test-runner
    - Comment out for now

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,23 +1,23 @@
-import { injectAxe, checkA11y } from 'axe-playwright';
+// import { injectAxe, checkA11y } from 'axe-playwright';
 
-import type { TestRunnerConfig } from '@storybook/test-runner';
+// import type { TestRunnerConfig } from '@storybook/test-runner';
 
-/*
- * See https://storybook.js.org/docs/react/writing-tests/test-runner#test-hook-api-experimental
- * to learn more about the test-runner hooks API.
- */
-const a11yConfig: TestRunnerConfig = {
-  async preRender(page) {
-    await injectAxe(page);
-  },
-  async postRender(page) {
-    await checkA11y(page, '#root', {
-      detailedReport: true,
-      detailedReportOptions: {
-        html: true
-      }
-    });
-  }
-};
+// /*
+//  * See https://storybook.js.org/docs/react/writing-tests/test-runner#test-hook-api-experimental
+//  * to learn more about the test-runner hooks API.
+//  */
+// const a11yConfig: TestRunnerConfig = {
+//   async preRender(page) {
+//     await injectAxe(page);
+//   },
+//   async postRender(page) {
+//     await checkA11y(page, '#root', {
+//       detailedReport: true,
+//       detailedReportOptions: {
+//         html: true
+//       }
+//     });
+//   }
+// };
 
-module.exports = a11yConfig;
+// module.exports = a11yConfig;
