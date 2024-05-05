c94abd189f1de8e42c5773943c59f1fd45cb7802
Lee Mills <8024370+millnut@users.noreply.github.com>
2024-05-05 10:44:53 +0100

    feat: added test mp4 video

--- src/components/atoms/Video/Video.stories.tsx
+++ src/components/atoms/Video/Video.stories.tsx
@@ -15,7 +15,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    videoSrc: 'myVideo.mp4',
+    videoSrc: 'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4',
     posterImage: ''
   }
 };
