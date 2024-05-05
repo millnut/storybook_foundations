ac313eb7bc29a852118343aa6ed40967437db564
Lee Mills <8024370+millnut@users.noreply.github.com>
2024-05-05 16:27:01 +0100

    chore: move accordionitem out of nesting

--- src/components/molecules/Accordion/index.tsx
+++ src/components/molecules/Accordion/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import AccordionItem, { AccordionItemProps } from './AccordionItem';
+import AccordionItem, { AccordionItemProps } from '../AccordionItem';
 import styles from './Accordion.module.scss';
 
 export interface AccordionProps {
