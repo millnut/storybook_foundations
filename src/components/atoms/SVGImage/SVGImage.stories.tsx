b4b01e431a995a925df402cc6ebf5118373fab1b
Lee Mills <8024370+millnut@users.noreply.github.com>
2024-05-05 10:19:53 +0100

    feat: svgimage atom

---
+++ src/components/atoms/SVGImage/SVGImage.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SVGImage from '.';
+
+const meta = {
+  title: 'Atoms/SVGImage',
+  component: SVGImage,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+} satisfies Meta<typeof Image>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    src: 'https://placeholder.pics/svg/500x500',
+    width: 500,
+    height: 500,
+    altText: 'An example image'
+  }
+};
---
+++ src/components/atoms/SVGImage/index.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from 'react';
+import styles from './SVGImage.module.scss';
+
+export interface SVGImageProps {
+  /**
+   * Source url of the image
+   */
+  src: string;
+  /*
+   * Width of image to stop layout shift
+   */
+  width: number;
+  /*
+   * Height of image to stop layout shift
+   */
+  height: number;
+  altText: string;
+  /*
+   * The loading attribute is used to lazy load the image. If you need the image
+   * to load quickly (example on a hero) then set loadingAttr to "eager"
+   */
+  loadingAttr?: 'lazy' | 'eager' | undefined;
+}
+
+export default function Image({ src, width, height, altText, loadingAttr = 'lazy' }: SVGImageProps): ReactElement {
+  return (
+    <img className={styles.svgimage} src={src} width={width} height={height} alt={altText} loading={loadingAttr} />
+  );
+}
