bb4f925ede7083797acb801abf7a0448cdf11e11
Lee Mills <8024370+millnut@users.noreply.github.com>
2025-10-27 17:23:51 +0000

    chore: update dependencies

--- src/types/declaration.d.ts
+++ src/types/declaration.d.ts
@@ -2,3 +2,8 @@ declare module '*.scss' {
   const styles: { [key: string]: string };
   export default styles;
 }
+
+declare module '*.module.scss' {
+  const styles: { [key: string]: string };
+  export default styles;
+}
